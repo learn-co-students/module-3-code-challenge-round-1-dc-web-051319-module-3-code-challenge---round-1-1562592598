0fdc50bf5a2f62d66f3521ec39dc4ff3a20a29b6
Peter Viss <peter.c.viss@biola.edu>
2019-07-08 10:45:53 -0400

    done cleanly

--- src/index.js
+++ src/index.js
@@ -11,31 +11,54 @@ document.addEventListener('DOMContentLoaded', () => {
 
   getImage()
 
-  likeButton()
+////////////////////////////GETTING HTML///////////////////////////////
+function getUl(){
+  return document.getElementById('comments')
+}
 
-  commentSubmit()
+function getImgName(){
+  return document.getElementById('name')
+}
+
+function getImgLikes(){
+  return document.getElementById('likes')
+}
+
+function getImg(){
+  return document.getElementById("image")
+}
 
 function getImage(){
-  fetch(`https://randopic.herokuapp.com/images/${imageId}`)
+  fetch(imageURL)
+    .then(resp => resp.json())
+    .then(data => retrieveImg(data))
+}
+
+////////////////////////////GETTING ORIGINAL IMAGE///////////////////////////////
+function getImage(){
+  fetch(imageURL)
     .then(resp => resp.json())
     .then(data => retrieveImg(data))
 }
 
 function retrieveImg(pic){
-  let img = document.getElementById("image")
-  let name = document.getElementById('name')
-  let likes = document.getElementById('likes')
-  let ul = document.getElementById('comments')
+  let img = getImg()
+  let name = getImgName()
+  let likes = getImgLikes()
+  let ul = getUl()
 
   img.src = pic.url
   name.innerText = pic.name
   likes.innerText = pic.like_count
   getComments(pic, ul)
 
+  likeButton()
+
+  commentSubmit()
   
 }
 
-
+///////////////////GETTING COMMENTS FROM IMAGE//////////////////////////
 function getComments(pic, ul){
   let li = document.createElement('li')
   for(let i = 0; i < pic.comments.length; i++){
@@ -46,14 +69,18 @@ function getComments(pic, ul){
  return ul
 }
 
+
+//////////////////////GETTING LIKE BUTTON W/ EVENT LISTENER//////////////////////
 function likeButton(){
  let likeBtn = document.getElementById('like_button')
  likeBtn.addEventListener('click', (e) => increaseLikes(e))
 }
 
+
+/////////////////////NEW LIKE TO DOM///////////////////////////////////
 function increaseLikes(e){
   e.preventDefault()
-  let likes = document.getElementById('likes')
+  let likes = getImgLikes()
   nuLike = parseInt(likes.innerText)
   nuLike = nuLike + 1
   sendLikeToDom(nuLike)
@@ -61,8 +88,10 @@ function increaseLikes(e){
   
 }
 
+
+////////////////////////NEW LIKE TO DATABASE//////////////////////////
 function sendLikeToDom(nuLike){
-  fetch(`https://randopic.herokuapp.com/likes/`, {
+  fetch(likeURL, {
     method: 'POST', 
       headers: {
         'Content-Type': 'application/json',
@@ -77,12 +106,16 @@ function sendLikeToDom(nuLike){
 }
 
 
+
+
+//////////////////////////NEW COMMENTS TO DOM//////////////////////////
 function commentSubmit(){
   let commentForm = document.getElementById('comment_form')
-  return commentForm.addEventListener('submit',  addCommentToDom)
+  return commentForm.addEventListener('submit', (e) => addCommentToDom(e, commentForm))
+  
 }
 
-function addCommentToDom(e){
+function addCommentToDom(e, commentForm){
   e.preventDefault()
   let ul = document.getElementById('comments')
   let li = document.createElement('li')
@@ -90,13 +123,14 @@ function addCommentToDom(e){
   li.innerText = content
   ul.appendChild(li)
   postComment(content)
+  e.target.children[0].value = ''
   return ul
 }
 
 
+/////////////////////NEW COMMENT TO DATABASE////////////////////////////////
 function postComment(content){
-  
-  fetch( `https://randopic.herokuapp.com/comments/`,{
+  fetch(commentsURL,{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
